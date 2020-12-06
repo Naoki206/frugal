c8e54b739b7426f7e0b24677fd3f1577e8a7fad1
鎌塚直己 <kamatsukanaoki@kamatsukanaokinoMacBook-Air.local>
2020-12-07 01:14:43 +0900

    initial_setting controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 use Illuminate\Http\Request;
 
@@ -23,6 +25,12 @@ class HomeController extends Controller
      */
     public function index()
     {
+        //初期設定ができているかチェック入れる。できてない場合設定ページ(本当は認証確認後のみに入れたい)
+        $income = DB::table('users')->where('id', Auth::id())->value('income');
+        if ($income == null) {
+            return view('initial_setting');
+        };
         return view('home');
     }
+
 }
